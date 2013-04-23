e50de5fd3d1be50ec910eb71aa96362c03fd3e51
Camron Flanders <camron.flanders@gmail.com>
2013-04-23 12:32:11 -0400

    Changed font, outlined hours, new release

--- src/fill.c
+++ src/fill.c
@@ -3,6 +3,20 @@
 #include "pebble_fonts.h"
 #include <math.h>
 
+#define HOURS_X 0
+#define HOURS_Y 60 
+
+#define SHADOW_OFFSET_X 3
+#define SHADOW_OFFSET_Y 3
+
+#define OUTLINE_1_OFFSET_X -1
+#define OUTLINE_1_OFFSET_Y -1
+
+#define OUTLINE_2_OFFSET_X 1
+#define OUTLINE_2_OFFSET_Y -1
+
+#define OUTLINE_3_OFFSET_X -1
+#define OUTLINE_3_OFFSET_Y 1
 
 #define MY_UUID { 0xB5, 0xCA, 0xC1, 0x91, 0xA9, 0x9D, 0x40, 0x8D, 0xA2, 0x2D, 0xEE, 0xB1, 0x42, 0xC3, 0xCA, 0x00 }
 PBL_APP_INFO(MY_UUID,
@@ -15,7 +29,10 @@ Window window;
 
 Layer display_layer;
 TextLayer hours_layer;
-TextLayer hours_outline;
+TextLayer hours_shadow;
+TextLayer hours_outline1;
+TextLayer hours_outline2;
+TextLayer hours_outline3;
 
 void display_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
@@ -45,7 +62,10 @@ void display_layer_update_callback(Layer *me, GContext* ctx) {
     
 
     text_layer_set_text(&hours_layer, time_text);
-    text_layer_set_text(&hours_outline, time_text);
+    text_layer_set_text(&hours_shadow, time_text);
+    text_layer_set_text(&hours_outline1, time_text);
+    text_layer_set_text(&hours_outline2, time_text);
+    text_layer_set_text(&hours_outline3, time_text);
 
     if(t.tm_min > 0) {
         lines = floor(t.tm_min*2.8);
@@ -73,23 +93,50 @@ void handle_init(AppContextRef ctx) {
   display_layer.update_proc = &display_layer_update_callback;
   layer_add_child(&window.layer, &display_layer);
 
-  GFont hour_font = fonts_get_system_font(FONT_KEY_GOTHAM_42_BOLD);
+  //GFont hour_font = fonts_get_system_font(FONT_KEY_GOTHAM_42_BOLD);
+
+  resource_init_current_app(&APP_RESOURCES);
+  GFont hour_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_BEBAS_48));
+
+  text_layer_init(&hours_shadow, window.layer.frame);
+  text_layer_set_text_color(&hours_shadow, GColorBlack);
+  text_layer_set_font(&hours_shadow, hour_font);
+  text_layer_set_background_color(&hours_shadow, GColorClear);
+  layer_set_frame(&(hours_shadow.layer), GRect(HOURS_X + SHADOW_OFFSET_X, HOURS_Y + SHADOW_OFFSET_Y, 144, 84));
+  text_layer_set_text_alignment(&hours_shadow, GTextAlignmentCenter);
+  layer_add_child(&display_layer, &hours_shadow.layer);
+
+  text_layer_init(&hours_outline1, window.layer.frame);
+  text_layer_set_text_color(&hours_outline1, GColorBlack);
+  text_layer_set_font(&hours_outline1, hour_font);
+  text_layer_set_background_color(&hours_outline1, GColorClear);
+  layer_set_frame(&(hours_outline1.layer), GRect(HOURS_X + OUTLINE_1_OFFSET_X, HOURS_Y + OUTLINE_1_OFFSET_Y, 144, 84));
+  text_layer_set_text_alignment(&hours_outline1, GTextAlignmentCenter);
+  layer_add_child(&display_layer, &hours_outline1.layer);
+
+  text_layer_init(&hours_outline2, window.layer.frame);
+  text_layer_set_text_color(&hours_outline2, GColorBlack);
+  text_layer_set_font(&hours_outline2, hour_font);
+  text_layer_set_background_color(&hours_outline2, GColorClear);
+  layer_set_frame(&(hours_outline2.layer), GRect(HOURS_X + OUTLINE_2_OFFSET_X, HOURS_Y + OUTLINE_2_OFFSET_Y, 144, 84));
+  text_layer_set_text_alignment(&hours_outline2, GTextAlignmentCenter);
+  layer_add_child(&display_layer, &hours_outline2.layer);
+
+  text_layer_init(&hours_outline3, window.layer.frame);
+  text_layer_set_text_color(&hours_outline3, GColorBlack);
+  text_layer_set_font(&hours_outline3, hour_font);
+  text_layer_set_background_color(&hours_outline3, GColorClear);
+  layer_set_frame(&(hours_outline3.layer), GRect(HOURS_X + OUTLINE_3_OFFSET_X, HOURS_Y + OUTLINE_3_OFFSET_Y, 144, 84));
+  text_layer_set_text_alignment(&hours_outline3, GTextAlignmentCenter);
+  layer_add_child(&display_layer, &hours_outline3.layer);
 
   text_layer_init(&hours_layer, window.layer.frame);
-  text_layer_set_text_color(&hours_layer, GColorBlack);
+  text_layer_set_text_color(&hours_layer, GColorWhite);
   text_layer_set_font(&hours_layer, hour_font);
   text_layer_set_background_color(&hours_layer, GColorClear);
-  layer_set_frame(&(hours_layer.layer), GRect(0, 67, 124, 84));
+  layer_set_frame(&(hours_layer.layer), GRect(HOURS_X, HOURS_Y, 124, 84));
   text_layer_set_text_alignment(&hours_layer, GTextAlignmentCenter);
   layer_add_child(&display_layer, &hours_layer.layer);
-
-  text_layer_init(&hours_outline, window.layer.frame);
-  text_layer_set_text_color(&hours_outline, GColorWhite);
-  text_layer_set_font(&hours_outline, hour_font);
-  text_layer_set_background_color(&hours_outline, GColorClear);
-  layer_set_frame(&(hours_outline.layer), GRect(-3, 64, 124, 84));
-  text_layer_set_text_alignment(&hours_outline, GTextAlignmentCenter);
-  layer_add_child(&display_layer, &hours_outline.layer);
 }
 
 
