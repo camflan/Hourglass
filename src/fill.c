fc64fc38a31a1d4b04f857c59a3caf4897065db3
Camron Flanders <camron.flanders@gmail.com>
2013-04-23 11:25:04 -0400

    Adding 1.0 release, reverted fill.c back to non-inverted codebase
    Have split the code into 2 branches, inverted and non-inverted.

--- src/fill.c
+++ src/fill.c
@@ -7,16 +7,15 @@
 #define MY_UUID { 0xB5, 0xCA, 0xC1, 0x91, 0xA9, 0x9D, 0x40, 0x8D, 0xA2, 0x2D, 0xEE, 0xB1, 0x42, 0xC3, 0xCA, 0x00 }
 PBL_APP_INFO(MY_UUID,
              "Hourglass Fill", "Camron Flanders",
-             0, 8, /* App version */
+             1, 0, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_WATCH_FACE);
 
 Window window;
 
 Layer display_layer;
-Layer inverse_layer;
 TextLayer hours_layer;
-TextLayer hours_inverse;
+TextLayer hours_outline;
 
 void display_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
@@ -46,18 +45,13 @@ void display_layer_update_callback(Layer *me, GContext* ctx) {
     
 
     text_layer_set_text(&hours_layer, time_text);
-    text_layer_set_text(&hours_inverse, time_text);
+    text_layer_set_text(&hours_outline, time_text);
 
     if(t.tm_min > 0) {
         lines = floor(t.tm_min*2.8);
         GRect sand = GRect(0, 168 - lines, 144, lines);
 
         graphics_fill_rect(ctx, sand, 0, GCornerNone);
-        
-        int inverse_height = 168 - lines - 67;
-        inverse_height = inverse_height > 0 ? inverse_height : 0;
-        
-        layer_set_frame(&inverse_layer, GRect(0, 67, 144, inverse_height));
     }
 }
 
@@ -82,23 +76,20 @@ void handle_init(AppContextRef ctx) {
   GFont hour_font = fonts_get_system_font(FONT_KEY_GOTHAM_42_BOLD);
 
   text_layer_init(&hours_layer, window.layer.frame);
-  text_layer_set_text_color(&hours_layer, GColorWhite);
+  text_layer_set_text_color(&hours_layer, GColorBlack);
   text_layer_set_font(&hours_layer, hour_font);
   text_layer_set_background_color(&hours_layer, GColorClear);
-  layer_set_frame(&(hours_layer.layer), GRect(0, 67, 144, 84));
+  layer_set_frame(&(hours_layer.layer), GRect(0, 67, 124, 84));
   text_layer_set_text_alignment(&hours_layer, GTextAlignmentCenter);
   layer_add_child(&display_layer, &hours_layer.layer);
-  
-  layer_init(&inverse_layer, GRect(0, 67, 144, 0));
-  layer_add_child(&window.layer, &inverse_layer);
 
-  text_layer_init(&hours_inverse, window.layer.frame);
-  text_layer_set_text_color(&hours_inverse, GColorBlack);
-  text_layer_set_font(&hours_inverse, hour_font);
-  text_layer_set_background_color(&hours_inverse, GColorClear);
-  layer_set_frame(&(hours_inverse.layer), GRect(0, 0, 144, 84));
-  text_layer_set_text_alignment(&hours_inverse, GTextAlignmentCenter);
-  layer_add_child(&inverse_layer, &hours_inverse.layer);
+  text_layer_init(&hours_outline, window.layer.frame);
+  text_layer_set_text_color(&hours_outline, GColorWhite);
+  text_layer_set_font(&hours_outline, hour_font);
+  text_layer_set_background_color(&hours_outline, GColorClear);
+  layer_set_frame(&(hours_outline.layer), GRect(-3, 64, 124, 84));
+  text_layer_set_text_alignment(&hours_outline, GTextAlignmentCenter);
+  layer_add_child(&display_layer, &hours_outline.layer);
 }
 
 
@@ -114,3 +105,4 @@ void pbl_main(void *params) {
 
   app_event_loop(params, &handlers);
 }
+
